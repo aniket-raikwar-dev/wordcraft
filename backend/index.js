246153388ce882be70bde4ba9c9ce26686b64e9f
aniket-raikwar <aniket@york.ie>
2024-03-30 17:41:40 +0530

    resolved cors

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,11 @@ const app = express();
 
 // Enable CORS for all routes
 app.use(cors());
+app.use(
+  cors({
+    origin: "https://wordcraft-weld.vercel.app/login",
+  })
+);
 
 // work like body-parser
 app.use(express.json());
